638c01bdb05ae3ca1e6e518ce7c113b7bb584db2
josephomenda <52826659+josephomenda@users.noreply.github.com>
2020-12-10 15:52:03 +0300

    Install guinicorn

--- abctenant/settings/production.py
+++ abctenant/settings/production.py
@@ -133,6 +133,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-cdn-local")
 
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
