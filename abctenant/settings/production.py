3eef05d4323fc7f227dcf97ee37e77ed90760547
josephomenda <52826659+josephomenda@users.noreply.github.com>
2020-12-10 14:47:34 +0300

    production settings

--- abctenant/settings/production.py
+++ abctenant/settings/production.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'abctenant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 db_from_env = dj_database_url.config()
