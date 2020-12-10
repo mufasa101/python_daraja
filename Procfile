bd1f710f9285fa777b19715c16d7c45f8b14f930
josephomenda <52826659+josephomenda@users.noreply.github.com>
2020-12-10 18:35:20 +0300

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: gunicorn daraja_python.wsgi --log-file -
+
+web: gunicorn daraja_python.wsgi:application --log-file - --log-level debug
+python manage.py collectstatic --noinput
+manage.py migrate
--- abctenant/settings/production.py
+++ abctenant/settings/production.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://serene-coast-15572.herokuapp.com/"]
+ALLOWED_HOSTS = ["https://serene-coast-15572.herokuapp.com",]
 
 
 # Application definition
